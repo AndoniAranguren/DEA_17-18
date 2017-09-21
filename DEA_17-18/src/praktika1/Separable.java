cedf3dd560dc454d8770aad32583570b89c5ed8d
Gorka <Gorka@10.109.144.197>
2017-09-21 12:56:04 +0200

    Metodo berria

--- DEA_17-18/src/praktika1/Separable.java
+++ DEA_17-18/src/praktika1/Separable.java
@@ -19,4 +19,28 @@ public class Separable {
 		}
 		return (left!=right ? 0 : indL); //(left ezberdin right)=true bada 0 bueltatzen du false bada indL bueltatzen du
 	}
+	public int esSeparable2(Integer[] tabla){
+		int izq=0, der=0, i=0, k=0;
+		boolean aurkituta=false;
+		boolean amaitu=false;
+		
+		while (i<tabla.length-1){
+			der=der+tabla[i];
+			i++;
+		}
+		while (aurkituta==false && amaitu==false){
+			izq=izq+tabla[k];
+			der=der-tabla[k];
+			if (izq>der || k>tabla.length){
+				amaitu=true;
+			}
+			if (izq==der){
+				aurkituta=true;
+			}
+			else 
+				k++;
+		}
+		if(aurkituta) return k;
+		else return 0;
+	}
 }
