43c12878824637e89338ad706d6fe00929911af7
Gorka <Gorka@LAPTOP-IM1K99J9>
2017-10-27 00:27:08 +0200

    REhecho

--- DEA_17-18/src/eginkizuna2/DoubleLinkedList.java
+++ DEA_17-18/src/eginkizuna2/DoubleLinkedList.java
@@ -27,24 +27,91 @@ public class DoubleLinkedList<T> implements ListADT<T> {
 	public T removeFirst() {
 	// listako lehen elementua kendu da
 	// Aurrebaldintza: zerrenda ez da hutsa
-		// KODEA OSATU ETA KOSTUA KALKULATU
-	}
+		T elem=null;
+		Node<T> current=null;
+		Node<T> last=first.prev;
+		if (first.next==null){ //listan elementu bakarra
+			elem=first.data;
+			first=null;
+		}
+//		else{                        BI ELEMENTU LISTAN SOILIK
+//			last=first.prev;
+//			current=first.next;
+//			elem=current.data;
+//			last.next=null;
+//			current.prev=null;
+//		}
+		else{ 				//Puntero batek lista borobila izatean berari apunta dakioke .next eta .prev - ekin?
+			elem=first.data;
+			current=first.next;
+			last.next=current; 
+			current.prev=last;
+		}
+		return elem;
+	} // O(1)
 
 	public T removeLast() {
 	// listako azken elementua kendu da
 	// Aurrebaldintza: zerrenda ez da hutsa
-		// KODEA OSATU ETA KOSTUA KALKULATU
-    }
+		
+		//CASO DE 2 ELEMENTOS??
+		T elem=null;
+		Node<T> current=null;
+		Node<T> last=first.prev;
+		if (first.next==null){ //listan elementu bakarra
+			elem=first.data; 
+			first=null;
+		}
+		else{
+			elem=last.data;
+			current=last.prev;
+			current.next=first; 
+			first.prev=current;
+		}
+		return elem;
+    }	// O(1)
 
 
-	public T remove(T elem) {
+	public T remove(T elem) { 	
 	// Aurrebaldintza: zerrenda ez da hutsa
 	// Balio hori listan baldin badago, bere lehen agerpena ezabatuko dut. Kendutako objektuaren erreferentzia 
-        //  bueltatuko du (null ez baldin badago)
-
-	
-		// KODEA OSATU ETA KOSTUA KALKULATU
-        };
+        
+		//  bueltatuko du (null ez baldin badago)
+		Node<T> current=null;
+		Node<T> aux=null;
+		Node<T> last=first.prev;
+		int kont=0;
+		boolean agerpen=false;
+		while (kont!=count && agerpen==false){
+			if (current.data==elem){
+				if (first.next==null){ //listan elementu bakarra 
+					first=null;
+					agerpen=true;
+				}
+				else if (current==first){ //listako lehenengo elementua da
+					current=current.next;
+					last.next=current; 
+					current.prev=last;
+					agerpen=true;
+				}
+				else if (current.next==first){ //listako azkenengo elementua
+					current=current.prev;
+					current.next=first;
+					first.prev=current;
+					agerpen=true;
+				} //listako beste elementuak
+				aux=aux.prev;
+				aux.next=current.next;
+				current=current.next; //si borras el current queda en el limbo??
+				current.prev=aux;
+				agerpen=true;
+			}
+			current=current.next;
+			aux=aux.next;
+			kont++;
+		}
+		return elem;
+	} 	// O(n)
 
 	public T first() {
 	// listako lehen elementua ematen du
@@ -62,16 +129,31 @@ public class DoubleLinkedList<T> implements ListADT<T> {
 
 	public boolean contains(T elem) {
 	// Egiazkoa bueltatuko du aurkituz gero, eta false bestela
-		      if (isEmpty())
-		          return false;
-
-		      		// KODEA OSATU ETA KOSTUA KALKULATU
+		Node<T> current=first;
+		Node<T> last=first.prev;
+		int kont=0;
+		boolean aurkituta=false;
+		if (isEmpty()){
+			return false;
+		}
+		else{
+		     while (kont!=count && aurkituta==false){
+		    	 if (last.data==elem){ //azkenengo elementua da
+		 			aurkituta=true;
+		    	 }
+		    	 else if (current.data==elem){
+		    		 aurkituta=true;
+		    	 }
+		    	 current=current.next;
+		    	 kont++;
+		     }
 		   }
+		return aurkituta;
+	}	//O(n)
 
 	public T find(T elem) {
 	// Elementua bueltatuko du aurkituz gero, eta null bestela
-
-		// KODEA OSATU ETA KOSTUA KALKULATU
+		return (contains(elem)? elem : null);
 	}
 
 	public boolean isEmpty() 
@@ -85,8 +167,32 @@ public class DoubleLinkedList<T> implements ListADT<T> {
 
 	   // an iterator, doesn't implement remove() since it's optional 
 	   private class ListIterator implements Iterator<T> { 
-
-		// KODEA OSATU 
+		   Node<T> current=null;
+		   T elem;
+		   
+		   public boolean hasNext(){
+			   if (current.next==first){
+				   return false;
+			   }
+			   else{
+				   return true;
+			   }
+		   }
+		   
+		   public T next(){
+			   if (!hasNext()){
+				   throw new NoSuchElementException();
+			   }
+			   else{
+				   elem=current.data;
+				   current=current.next;
+			   }
+			   return elem;
+		   }
+		   
+		   public void remove(){
+			   throw new UnsupportedOperationException("an iterator, doesn't implement remove() since it's optional");
+		   }
 	   } // private class
 		
 		
