1ff7d181a58c9aa196a6b8d9f245b02e43bdff65
Urko <Urko@Urko>
2017-11-01 19:09:29 +0100

    ordered/unordered

--- DEA_17-18/src/eginkizuna2/OrderedDoubleLinkedList.java
+++ DEA_17-18/src/eginkizuna2/OrderedDoubleLinkedList.java
@@ -2,11 +2,40 @@ package eginkizuna2;
 
 public class OrderedDoubleLinkedList<T> extends DoubleLinkedList<T> implements OrderedListADT<T> {
 	
-	public void add(T elem){
+	public void add(T elem){//compareTo ez da atzigarria T elementuekin, orduan bakoitza bere hasode-ari begira ordenatuko da, bakoitzak bakarra izango baitu, .equals==true ematen ez den bitartean.
 		// KODEA OSATU ETA KOSTUA KALKULATU
-
-
-
+		Node<T> sartu= new Node<T>(elem);
+		boolean sartuta=false;
+		if (first.equals(null)) first=sartu;
+		else if(first.data.hashCode()>sartu.data.hashCode()){//lehenengoa
+			sartu.next=first;
+			sartu.prev=first.prev;
+			first.prev.next=sartu;
+			first.prev=sartu;
+			first=sartu;
+			sartuta=true;
+		}
+		else{
+			Node<T> current=first.next;
+			while(!current.equals(first)&&!sartuta){
+				if(current.data.hashCode()>sartu.data.hashCode()){//erditik
+					sartu.prev=current.prev;
+					sartu.next=current;
+					current.prev.next=sartu;
+					current.prev=sartu;
+					sartuta=true;
+				}
+				current=current.next;
+			}
+			if(!sartuta){//azkena
+				sartu.next=first;
+				sartu.prev=first.prev;
+				first.prev.next=sartu;
+				first.prev=sartu;
+				sartuta=true;
+			}
+		}
+		if (!sartuta) System.out.println("Sartu nahi duzun datua badago jada listan.");
 	}
 
 
--- DEA_17-18/src/eginkizuna2/UnorderedDoubleLinkedList.java
+++ DEA_17-18/src/eginkizuna2/UnorderedDoubleLinkedList.java
@@ -2,20 +2,54 @@ package eginkizuna2;
 
 public class UnorderedDoubleLinkedList<T> extends DoubleLinkedList<T> implements UnorderedListADT<T> {
 	
+	private Node<T> first; // lehenengoaren erreferentzia
+	private String deskr;  // deskribapena
+	private int count;
+	
+	
 	public void addToFront(T elem) {
 	// hasieran gehitu
-		// KODEA OSATU ETA KOSTUA KALKULATU
-
+		Node<T> sartu=new Node<T> (elem) ;
+		if (first.equals(null)) first=sartu;
+		else{
+			sartu.next=first;
+			sartu.prev=first.prev;
+			first.prev=sartu;
+			first=sartu;
+		}
 	}
 
 	public void addToRear(T elem) {
 	// bukaeran gehitu
-		// KODEA OSATU ETA KOSTUA KALKULATU
-
+		Node<T> sartu=new Node<T> (elem) ;
+		if (first.equals(null)) first=sartu;
+		else{
+			sartu.next=first;
+			sartu.prev=first.prev;
+			first.prev.next=sartu;
+			first.prev=sartu;			
+		}
 	}
 	
 	public void addAfter(T elem, T target) {
 		// KODEA OSATU ETA KOSTUA KALKULATU (AUKERAZKOA)
+		Node<T> sartu=new Node<T> (elem) ;
+		if(first.equals(null)) first=sartu;
+		else{
+			Node<T> current= first.next;
+			while(!current.data.equals(target)&&!current.equals(first)){
+				current=current.next;
+			}
+			if (current.data.equals(target)){
+				if(current.equals(first)) addToFront(elem);
+				else if (current.equals(first.prev)) addToRear(elem);
+				else{
+					sartu.next=current.next;
+					sartu.prev=current;
+					current.next.prev=sartu;
+					current.next=sartu;
+				}
+			}
+		}
 	}
-
 }
