f4b0363835a641f9dd46c9d4b37af49c635292b7
Kaskagues <andoni.aranguren@gmail.com>
2017-11-21 11:45:12 +0100

    Grafoa + 3 proba kasu
    Linkeatuta daukan lista, ez linkeatuta daukana eta bere burura.
    
    +Probatu behar da lista handi batekin, eta konparatu beste
    algoritmoarekin. Zein da azkarragoa

--- DEA_17-18/src/eginkizuna3/Grafoa.java
+++ DEA_17-18/src/eginkizuna3/Grafoa.java
@@ -1,1 +1,170 @@
+package eginkizuna3;
 
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Scanner;
+
+public class Grafoa {
+	private HashMap<String,Integer> th;
+	private String[] keys;
+	private ArrayList<Integer>[] adjList;
+	private int totales;
+	
+	public Grafoa(){}
+	
+	public void init(String pWebOrriak, String pErlazioak) {		
+		th= new HashMap<String,Integer>();
+		totales= lineaTotalak(pWebOrriak);
+		keys= new String[totales];
+		adjList = new ArrayList[totales];
+		datuakKargatu(pWebOrriak);
+		webOrrienDatuakKargatu(pErlazioak);
+	}
+	private int lineaTotalak(String pHeblidea) {
+		int lines = 0;
+		try {
+			java.io.File file = new java.io.File(pHeblidea);
+			java.io.LineNumberReader lineNumberReader = new java.io.LineNumberReader(new FileReader(file));
+			lineNumberReader.skip(Long.MAX_VALUE);
+			lines = lineNumberReader.getLineNumber()+1;
+			lineNumberReader.close();
+		}catch(IOException e){
+			
+		}
+		return lines;
+	}
+	private void datuakKargatu(String pHelbidea){
+		String linea;
+		int kop=1,ehuneko=0;
+		boolean loading= totales>1000000;
+
+		System.out.println("WebOrriZerrendan datuak kargatzen...");
+		try{
+			Scanner entrada= new Scanner (new FileReader(pHelbidea)); //Scanner solo coge string, toda la linea
+			if(loading) {
+				System.out.print("[");
+				for(int i =0; i<100;i++) System.out.print("-");
+				System.out.println("]");
+				System.out.print("[");
+			}
+			while(entrada.hasNext()){
+				if(loading&&ehuneko!=(kop* 100)/totales) {
+					ehuneko=(kop* 100)/totales;
+					System.out.print("=");
+				}
+				linea=entrada.nextLine();
+				
+				String url=linea.split(" ")[0];
+				int id=Integer.parseInt(linea.split(" ")[1]);
+				th.put(url,id);
+				keys[id]=url;
+						
+				kop++;
+			}
+			entrada.close();
+			if(loading)System.out.println("]");
+		}
+		catch(IOException e){System.out.println("Beste helbide bat sar ezazu.");}
+
+		System.out.println((kop-1)+ " weborri kargatu dira");
+	}
+	
+	private void webOrrienDatuakKargatu(String pHelbidea){
+		String linea;
+		int nondik, nora;
+		int totales= lineaTotalak(pHelbidea), kop=1,ehuneko=0;
+		boolean loading= totales>1000000;
+		try{
+			Scanner entrada= new Scanner (new FileReader (pHelbidea));
+			if(loading) {
+				System.out.print("[");
+				for(int i =0; i<100;i++) System.out.print("-");
+				System.out.println("]");
+				System.out.print("[");
+			}
+			while(entrada.hasNext()){
+				if(loading&&ehuneko!=(kop* 100)/totales) {
+					ehuneko=(kop* 100)/totales;
+					System.out.print("=");
+				}
+				linea=entrada.nextLine();
+				
+				nondik=Integer.parseInt(linea.split(" ")[0]);
+				nora=Integer.parseInt(linea.split(" ")[1]);
+				if(adjList[nondik]==null)
+					adjList[nondik]= new ArrayList<Integer>();
+				adjList[nondik].add(nora);
+				
+				kop++;
+			}
+			entrada.close();
+			if(loading)System.out.println("]");
+		}
+		catch(IOException e){
+			System.out.println("Sar ezazu beste helbide bat.");
+			}
+
+		System.out.println((kop-1)+ " weborri estekatu dira");
+	}
+	
+	public String toString() {
+		String retString = "";
+		if(keys!=null)
+			for(String key: keys) {
+				retString= retString.concat("\nID: "+ th.get(key) +" ("+ key+")  goes to:");
+				if(adjList[th.get(key)]==null)
+					retString= retString.concat(" nowhere");
+				else
+					for(int i: adjList[th.get(key)])
+						retString= retString.concat(" "+i);
+			}
+		return retString;
+	}
+	
+	public ArrayList<String> erlazionatutaPath(String a1, String a2){
+		ArrayList<String> retArray= null;
+
+		HashMap<String, String> path = new HashMap<String,String>();
+		eginkizuna2.UnorderedDoubleLinkedList<Integer> nextWebs= 
+				new eginkizuna2.UnorderedDoubleLinkedList<Integer>();
+		
+		int currentWeb=th.get(a1);
+		int destiny= th.get(a2);
+		boolean found=false;
+		
+		nextWebs.addToRear(currentWeb);
+		path.put(keys[currentWeb], null);
+		
+		while(!found && !nextWebs.isEmpty()) {
+			currentWeb=nextWebs.first();
+			found= (currentWeb==destiny);
+			if(!found) {
+				nextWebs.removeFirst();
+				if(adjList[currentWeb]!=null)
+					for(int id:adjList[currentWeb]) {
+						if(!path.containsKey(keys[id])){
+							nextWebs.addToRear(id);
+							path.put(keys[id], keys[currentWeb]);
+						}
+					}
+			}
+		}
+		
+		if(found) {//Create the array we have to return
+			String current= a2;
+			retArray= new ArrayList<String>();
+			while(current!=null) {	//Inverted array. O(m) m being the amount of webs linked between a1 and a2
+				retArray.add(current);
+				current=path.get(current);
+			}
+			for (int i = 0; i < retArray.size() / 2; ++i) { // Invert it back. O(m/2) only goes for half the array size
+			    String elem = retArray.get(i); 
+			    retArray.set(i,retArray.get( (retArray.size()-1) -i )); // we set i-th element with i-th element from the back
+			    retArray.set( (retArray.size()-1) -i, elem);
+			  }
+		}
+		return retArray;
+	}
+}
--- DEA_17-18/src/eginkizuna3/GrafoaTest.java
+++ DEA_17-18/src/eginkizuna3/GrafoaTest.java
@@ -1,1 +1,53 @@
+package eginkizuna3;
 
+import javax.swing.SingleSelectionModel;
+import javax.swing.plaf.synth.SynthSeparatorUI;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+public class GrafoaTest {
+
+	private Grafoa grafoa= new Grafoa();
+	
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception {
+	}
+
+	@AfterClass
+	public static void tearDownAfterClass() throws Exception {
+	}
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	@Test
+	public void init() {
+		grafoa.init("src\\eginkizuna1\\indexLaburra.txt", "src\\eginkizuna1\\pdl-arcLaburra.txt");
+	}
+	@Test
+	public void testErlazionatutaPath() {
+		grafoa.init("src\\eginkizuna1\\indexLaburra.txt", "src\\eginkizuna1\\pdl-arcLaburra.txt");
+		
+//		System.out.println(grafoa);
+		
+ 		String a1="0-00.pl";	//A web that links to a2
+ 		String a2="0-24.ro";	//A web that is linked by a1
+ 		String a3="0-360.com";	//A web that doesn't link to anybody
+ 		
+ 		Assert.assertNotNull(grafoa.erlazionatutaPath(a1, a2));	//Linkeatuta daukan web batera
+ 		Assert.assertNull(grafoa.erlazionatutaPath(a3, a2));	//Ez linkeatuta daukan web batera
+ 		Assert.assertEquals(grafoa.erlazionatutaPath(a1, a1).toString(),"["+a1+"]");	//Bere burura
+ 		
+	}
+
+}
